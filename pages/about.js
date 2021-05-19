53024761a8235965f91146630e327933b7d464f6
jun-young <jun094@naver.com>
2021-05-19 13:59:39 +0900

    Init folder

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function about() {
+    return <div>about페이지입니다.</div>;
+}
+
+export default about;
---
+++ pages/view/[id].js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function View() {
+    return <div>id</div>;
+}
+
+export default View;
