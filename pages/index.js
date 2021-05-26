f46b0e79c6a163610220c88e630b7c6f0ead664c
jun-young <jun094@naver.com>
2021-05-26 19:34:48 +0900

    get api

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,21 @@
 import Head from 'next/head';
+import Axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+    const [list, setList] = useState([]);
+
+    const API_URL = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline';
+
+    const getDatas = () => {
+        Axios.get(API_URL).then((res) => {
+            setList(res.data);
+            console.log(res.data);
+        });
+    };
+    useEffect(() => {
+        getDatas();
+    }, []);
     return (
         <>
             <Head>
---
+++ src/Components/ItemList.js
@@ -0,0 +1,5 @@
+function ItemList() {
+    return <img src="https://d3t32hsnjxo7q6.cloudfront.net/i/c924006882e8e313d445a3a5394e4729_ra,w158,h184_pa,w158,h184.png"></img>;
+}
+
+export default ItemList;
