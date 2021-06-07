1f9c43a633bf1262264198c3e95c40f7ea6de6b5
jun-young <jun094@naver.com>
2021-06-07 22:20:07 +0900

    complete gnv
    use useRouter of Next JS

--- src/Components/Gnb.js
+++ src/Components/Gnb.js
@@ -1,13 +1,30 @@
 import { Menu } from 'semantic-ui-react';
+import { useRouter } from 'next/router';
 
 function Gnb() {
-    const activeItem = 'home';
+    const router = useRouter();
+    let activeItem;
+
+    if (router.pathname === '/') {
+        activeItem = 'home';
+    } else if (router.pathname === '/about') {
+        activeItem = 'about';
+    }
+
+    //semantic-ui gnv 제공 기능
+    //menu.item 의 name --> data.name
+    const goLink = (e, data) => {
+        if (data.name === 'home') {
+            router.push('/');
+        } else if (data.name === 'about') {
+            router.push('/about');
+        }
+    };
 
     return (
         <Menu inverted>
-            <Menu.Item name="home" active={activeItem === 'home'} />
-            <Menu.Item name="messages" active={activeItem === 'messages'} />
-            <Menu.Item name="friends" active={activeItem === 'friends'} />
+            <Menu.Item name="home" active={activeItem === 'home'} onClick={goLink} />
+            <Menu.Item name="about" active={activeItem === 'about'} onClick={goLink} />
         </Menu>
     );
 }
